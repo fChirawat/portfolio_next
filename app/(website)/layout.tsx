fecb0bffd1af68cd50bde5d769f6c94e251a8c93
fChirawat <kang2499k11@gmail.com>
2024-11-30 19:34:32 +0700

    nice2

--- app/(website)/layout.tsx
+++ app/(website)/layout.tsx
@@ -1,8 +1,9 @@
-import Image from 'next/image'
+import Image from 'next/image';
+
 export default function LayoutWeb({
     children,
 }: {
-    children: React.ReactNode
+    children: React.ReactNode;
 }) {
     return (
         <>
@@ -10,14 +11,25 @@ export default function LayoutWeb({
                 <div className="container mx-auto h-full flex items-center">
                     <div className="w-1/5 flex flex-col justify-center items-center">
                         <div className="rounded-full w-[10rem] overflow-hidden shadow-lg">
-                            <img src="/img/me" alt="Profile" /> 
+                            <Image
+                                src="/img/Me.jpg"
+                                alt="Profile"
+                                width={160}
+                                height={160} 
+                            />
                         </div>
                         <p className="mt-4 font-bold text-lg">Chirawat Pingmueang</p>
                     </div>
                     <div className="w-4/5">
-                        <h1 className="text-4xl font-bold tracking-wide">Welcome to Our Website</h1>
+                        <h1 className="text-4xl font-bold tracking-wide">
+                            Lorem ipsum dolor sit amet consectetur, adipisicing 
+                        </h1>
                         <p className="mt-2 text-lg opacity-90">
-                            Enhance your experience with our modern and stylish design.
+                            <div className='flex'>
+                                <img className='w-[2rem] mx-2' src="https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg" alt="" /><a className="mr-5" href="https://github.com/fChirawat" target="_blank" rel="noopener noreferrer">Github</a>
+                                <img className='w-[2rem] mx-2' src="https://cdn4.iconfinder.com/data/icons/social-media-logos-6/512/112-gmail_email_mail-512.png" alt="" /><a className="mr-5" href="" target="" rel="noopener noreferrer">Kang2499k11@gmail.com</a>
+                                <img className='w-[2rem] mx-2' src="https://freesvg.org/img/molumen_phone_icon.png" alt="" /><a className="mr-5" href="tel:0890439908">+66 89-043-9908</a>
+                            </div>
                         </p>
                     </div>
                 </div>
--- app/(website)/page.tsx
+++ app/(website)/page.tsx
@@ -51,7 +51,7 @@ export default function Home() {
                     />
                   ))}
                 </div>
-              </div>
+              </div>  
             ))}
           </div>
         </div>
