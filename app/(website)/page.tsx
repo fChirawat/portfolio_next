74863e05d1c1b4bac732ae3be497ccf26af5eec4
fChirawat <kang2499k11@gmail.com>
2024-11-30 18:48:35 +0700

    ts1

--- app/(website)/page.tsx
+++ app/(website)/page.tsx
@@ -1,34 +1,61 @@
-export default function Home (){
-  return(
+export default function Home() {
+  return (
     <>
-      <children/>
-      {/* headbar */}
-      <div className="flex h-screen">
-                {/* Left Section: Pink Background */}
-                <div className="w-1/2 bg-pink-500 flex flex-col items-center justify-center">
-                    <ul className="h-1/3 mt-10">
-                        <li>
-                            <a href="/" className="text-white"><h1></h1></a>
-                        </li>
-                    </ul>
-                    <div className="h-2/3 flex items-center">
-                        <h1 className="text-3xl font-bold">แนะนำตัว</h1>
-                    </div>
+      <children />
+      <div className="bg-white text-gray-700">
+        <div className="container mx-auto  ">
+          <div className="grid grid-cols-12 ">
+            <div className="col-span-4">
+              <h1 className="mt-6 text-3xl ">Skill</h1>
+              <ul className="list-disc mt-6 mb-6    " >
+                <div className="flex ">
+                  <img className="rounded-full  w-[2rem] mx-2 my-1"src="https://cdn.worldvectorlogo.com/logos/html-1.svg"alt=""/>
+                  <img className="rounded-full  w-[2rem] mx-2 my-1"src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/CSS3_logo.svg/2048px-CSS3_logo.svg.png"alt=""/>
+                  <img className="rounded-full  w-[2rem] mx-2 my-1"src="https://cdn.worldvectorlogo.com/logos/javascript-1.svg"alt="" />
                 </div>
-
-                {/* Right Section: Blue Background */}
-                <div className="w-1/2 bg-blue-500 flex items-center justify-center">
-                    <ul>
-                        <li>
-                            <a href="/" className="text-white">Logo</a>
-                        </li>
-                    </ul>
+                <div className="flex mt-2">
+                  <img className="rounded-full  w-[2rem] mx-2 my-1"src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/2048px-Typescript_logo_2020.svg.png"alt=""/>
+                  <img className="rounded-full  w-[2rem] mx-2 my-1"src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1869px-Python-logo-notext.svg.png"alt=""/>
                 </div>
+              </ul>
             </div>
 
+            <div className="col-span-4">
+              <h1 className="mt-6 text-3xl">Framework</h1>
+              <ul className="list-disc mt-6 mb-6">
+                <div className="flex ">
+                  <img className="rounded-full  w-[2rem]  mx-2 my-1"src="https://www.logo.wine/a/logo/React_(web_framework)/React_(web_framework)-Logo.wine.svg"alt=""/>
+                  <img className="rounded-full  w-[2rem]  mx-2 my-1"src="https://img.icons8.com/color/200/vue-js.png" alt=""/>
+                  <img className="rounded-full  w-[2rem]  mx-2 my-1"src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/1280px-Bootstrap_logo.svg.png"alt="" />
+                </div>
+                <div className="flex  mt-2 ">
+                  <img className="rounded-full  w-[2rem] mx-2 my-1" src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Tailwind_CSS_Logo.svg/2560px-Tailwind_CSS_Logo.svg.png"alt=""/>
+                  <img className="rounded-full  w-[2rem] mx-2 my-1" src="https://seeklogo.com/images/N/nodejs-logo-FBE122E377-seeklogo.com.png" alt=""/>
+                  <img className="rounded-full  w-[2rem] mx-2 my-1" src='https://cdn.worldvectorlogo.com/logos/next-js.svg' alt="" />
+                </div>
+                <div className="flex   mt-2">
+                  <img className="rounded-full  w-[2rem] mx-2 my-1"   src="https://cdn.iconscout.com/icon/premium/png-256-thumb/express-js-5379348-4492470.png?f=webp"    alt=""/>
+                  <img className="rounded-full  w-[2rem] mx-2 my-1"  src="https://upload.wikimedia.org/wikipedia/commons/a/a8/NestJS.svg" />
+                </div>
+              </ul>
+            </div>  
 
-      <div><h1>about</h1></div>
-
+            <div className="col-span-4">
+              <h1 className="mt-6 text-3xl">Software Tools</h1>
+              <ul className="list-disc mt-6 mb-6">
+                <div className="flex  ">
+                  <img className="rounded-full  w-[2rem] mx-2 my-1"  src="https://upload.wikimedia.org/wikipedia/commons/3/33/Figma-logo.svg"   alt="" />
+                  <img className="rounded-full  w-[2rem] mx-2 my-1"  src="https://cdn.worldvectorlogo.com/logos/xampp.svg"  alt="" />
+                </div>
+                <div className="flex  mt-2">
+                  <img className="rounded-full  w-[2rem] mx-2 my-1" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/1200px-Octicons-mark-github.svg.png"    alt=""  />
+                  <img className="rounded-full  w-[2rem] mx-2 my-1"  src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/2048px-Visual_Studio_Code_1.35_icon.svg.png" alt=""  />
+                </div>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
